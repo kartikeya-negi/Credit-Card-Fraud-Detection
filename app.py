53bd125b90f4a69066697077190590eaf4e89c35
Kartikeya Negi <144468013+kartikeya-negi@users.noreply.github.com>
2025-06-25 23:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ st.write(
 
 # List of features as per your notebook
 feature_columns = [
+    'Time',  
     'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
     'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20',
     'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount'
 ]
+
 
 # Input fields for all features
 user_input = []
